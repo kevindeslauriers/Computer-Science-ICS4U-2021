78bcc74b92a8112f9d93c8999882885c1c3d6989
Kevin DesLauriers <kevindeslauriers@gmail.com>
2021-02-08 10:23:34 -0500

    moved numClasses as an instance variable

--- Unit One/src/assignment/Driver.java
+++ Unit One/src/assignment/Driver.java
@@ -12,6 +12,8 @@ public class Driver {
   private static QuestionBank qBank;
   private static Student[] students;
   private static Class[] classes;
+  private int numClasses = 0;
+  private int numStudents = 0;
 
 
   public Driver(){
@@ -30,49 +32,72 @@ public class Driver {
   }
 
 
-public static void main(String[] args) {
-  Driver driver = new Driver();
-
-  /**
-   * Create a menu with the following functionality.
-   * 1. Create new class
-   * 2. Create new student
-   * 3. Assign a student to a class
-   * 4. Create a new question to add to the question bank
-   * 5. Create a new assignment and assign to a class.
-   * 5a. Choose questions to add to the Assessment
-   * 6. Allow a student to take a test from a specific class.
-   * 7. Display a student's average.
-   * 8. Display a student's results for a specific course/class.
-   * 9. Display the average for all of the students in a class.
-   * 10. Display the average for a specific course.
-   * 11. Display all the students by grade (9-12)
-   * 12. Display all the students by cohort (A/B/O)
-   */
-}
-
-private static void loadAllClasses(String fileName) {
-  try{
-    Scanner scanner = new Scanner(new File(fileName));
-    int numClasses = 0;
-    classes = new Class[MAX_CLASSES];
-    while(scanner.hasNextLine()){
-      String name = scanner.nextLine().split(":")[1].trim();
-      String courseCode = scanner.nextLine().split(":")[1].trim();
-
-      Class c = new Class(name, courseCode);
-      classes[numClasses] = c;
-      numClasses++;
+  public static void main(String[] args) {
+    Driver driver = new Driver();
+    boolean done = false;
+    Scanner in = new Scanner(System.in);
+    while(!done){
+      int option = Integer.parseInt(in.nextLine());
+      displayMenu();
+      if (option == 1){
+        addClass();
+      }else if (option == 2){
+        addStudent();
+      }
     }
-  }catch(FileNotFoundException ex){
+    /**
+     * Create a menu with the following functionality.
+     * 1. Create new class
+     * 2. Create new student
+     * 3. Assign a student to a class
+     * 4. Create a new question to add to the question bank
+     * 5. Create a new assignment and assign to a class.
+     * 5a. Choose questions to add to the Assessment
+     * 6. Allow a student to take a test from a specific class.
+     * 7. Display a student's average.
+     * 8. Display a student's results for a specific course/class.
+     * 9. Display the average for all of the students in a class.
+     * 10. Display the average for a specific course.
+     * 11. Display all the students by grade (9-12)
+     * 12. Display all the students by cohort (A/B/O)
+     */
+  }
+
+  private static void addStudent() {
+  }
+
+  private static void addClass() {
+  
+  }
+
+  private static void displayMenu() {
+    System.out.println("1. Create Class");
+    System.out.println("1. Create Student");
 
   }
 
+  private static void loadAllClasses(String fileName) {
+    try{
+      Scanner scanner = new Scanner(new File(fileName));
+      
+      classes = new Class[MAX_CLASSES];
+      while(scanner.hasNextLine()){
+        String name = scanner.nextLine().split(":")[1].trim();
+        String courseCode = scanner.nextLine().split(":")[1].trim();
 
-}
+        Class c = new Class(name, courseCode);
+        classes[numClasses] = c;
+        numClasses++;
+      }
+    }catch(FileNotFoundException ex){
 
-private static void loadAllStudents(String string) {
-  
-}
+    }
+
+
+  }
+
+  private static void loadAllStudents(String string) {
+    
+  }
   
 }
