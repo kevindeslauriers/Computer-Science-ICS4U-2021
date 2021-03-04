bad92ca3808d053e4dd24f643638368735ab803b
Kevin DesLauriers <kevindeslauriers@gmail.com>
2021-03-04 10:45:35 -0500

    NQueens Assignment Stubs

---
+++ Unit One/src/week6/NQueens.java
@@ -0,0 +1,8 @@
+public class NQueens {
+  public static void main(String[] args) {
+    solveNQueens(4);
+  }
+
+  private static void solveNQueens(int n) {
+  }
+}
---
+++ Unit One/src/week6/Queen.java
@@ -0,0 +1,28 @@
+package week6;
+
+public class Queen {
+  private int row;
+  private int col;
+
+  public int getRow() {
+    return row;
+  }
+
+  public void setRow(int row) {
+    this.row = row;
+  }
+
+  public int getCol() {
+    return col;
+  }
+
+  public void setCol(int col) {
+    this.col = col;
+  }
+
+  public Queen(int row, int col) {
+    this.row = row;
+    this.col = col;
+  }
+
+}
