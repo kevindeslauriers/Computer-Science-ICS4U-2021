36306f211504563601694241ef06772880fbd628
Kevin DesLauriers <kevindeslauriers@gmail.com>
2021-03-04 08:56:08 -0500

    Array implemetation of Stack (Stub only)

---
+++ Unit One/src/week6/Stack.java
@@ -0,0 +1,39 @@
+package week6;
+
+/* An Array implementation of the stack where manyItems-1 is the top */
+public class Stack {
+  int[] data;
+  int manyItems;
+  private static final int DEFAULT_CAPACITY = 10;
+
+  public Stack() {
+    data = new int[DEFAULT_CAPACITY];
+    manyItems = 0;
+  }
+
+  public Stack(int initialCapacity) {
+    data = new int[initialCapacity];
+    manyItems = 0;
+  }
+
+  public boolean isEmpty() {
+
+  }
+
+  public void push(int el) {
+
+  }
+
+  public int peek() {
+
+  }
+
+  public int pop() {
+
+  }
+
+  public int search(int el) {
+
+  }
+
+}
