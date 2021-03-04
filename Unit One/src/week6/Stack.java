4282e2a1b0f56c17e525140ce8d705811c9b8c45
Kevin DesLauriers <kevindeslauriers@gmail.com>
2021-03-04 09:06:55 -0500

    Array implementation of Stack

--- Unit One/src/week6/Stack.java
+++ Unit One/src/week6/Stack.java
@@ -17,23 +17,41 @@ public class Stack {
   }
 
   public boolean isEmpty() {
-
+    return manyItems == 0;
   }
 
   public void push(int el) {
-
+    if (manyItems == data.length)
+      throw new IllegalStateException();
+    data[manyItems] = el;
+    manyItems++;
   }
 
   public int peek() {
-
+    if (!isEmpty())
+      return data[manyItems - 1];
+    else
+      throw new IllegalStateException();
   }
 
   public int pop() {
-
+    if (!isEmpty()) {
+      manyItems--;
+      return data[manyItems];
+    } else
+      throw new IllegalStateException();
   }
 
   public int search(int el) {
+    int depth = 0;
+    for (int i = manyItems - 1; i >= 0; i--) {
+      if (data[i] == el)
+        return depth;
+      else
+        depth++;
+    }
 
+    return -1;
   }
 
 }
